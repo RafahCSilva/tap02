fa09db017d69a53ad45920bb037970bff574d4b7
RafahCSilva <rafahcsilva@gmail.com>
2018-03-25 15:16:18 -0300

    install pj

---
+++ src/rafaelcardoso_tap02.cpp
@@ -0,0 +1,17 @@
+/**
+ * @author Rafael Cardoso da Silva 21048012
+ * Tecnicas Avancadas de Programacao – 2018.Q1
+ * Projeto 2: Domino
+ *
+ * Compile com C++ 11:
+ *   g++ rafaelcardoso_tap02.cpp -o rafaelcardoso_tap02.exe -std=c++11
+ */
+
+#include <iostream>
+
+
+int main() {
+    std::cout << "eae Rafao" << std::endl;
+}
+
+
