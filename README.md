e570906d9c5eb6b99fd741fef3d855dead839e2a
RafahCSilva <rafahcsilva@gmail.com>
2018-03-26 03:13:59 -0300

    UHUULL 11.84 ptos
    Tempo decorrido: 03:33
    
    Nota do item (a): 7.92 de 8
    Nota do item (b): 3.92 de 4
    Nota do item (c): 0.00 de 4
    
    Nota do projeto: 11.84 de 10

--- README.md
+++ README.md
@@ -2,10 +2,26 @@
 
 ## Projeto 2: Trilha de Dominós!
 
+Build e execute Corretor do Professor:
+
+```bash
+chmod +x RUN.sh
+./RUN.sh
+```
+
+Build manual:
 
 ```bash
 cd src/
-g++ rafaelcardoso_tap02.cpp -o rafaelcardoso_tap02.exe -std=c++11 && cp rafaelcardoso_tap02.exe ../scripts/
+g++ rafaelcardoso_tap02.cpp -o rafaelcardoso_tap02.exe -std=c++11
+cp rafaelcardoso_tap02.exe ../scripts/
 cd ../scripts/
 ./grade.bash ./rafaelcardoso_tap02.exe
 ```
+
+Monta a arvore de Recursao
+
+```bash
+tap02.exe 2> aa.txt < ..\scripts\in\file09
+```
+
--- src/rafaelcardoso_tap02.cpp
+++ src/rafaelcardoso_tap02.cpp
@@ -10,100 +10,126 @@
 #include <iostream>
 #include <vector>
 
+#define V false     // VERBOSE
+
+// FLAG para se a peca de Domino já estiver sendo usada ou nao
+#define PECA_NAO_USADA  0
+#define PECA_USADA      1
+
+/**
+ * Struct de uma Peca de Domino.
+ */
 struct peca_t {
-    int a, b, id;
+    int a, b, usado;
 };
 
-int N, K;
-std::vector<peca_t> M;
-// std::vector<peca_t> *S;
-peca_t *S;
-std::vector<std::vector<peca_t> > R;
+// Numero de Dominos
+int N;
 
-void print_pecas() {
-    fprintf(stderr, "\nPrint Pecas:\n" );
-    for ( peca_t p : M ) {
-        fprintf(stderr, "  %d: %d - %d\n", p.id, p.a, p.b );
-    }
-}
+// Numero de Casa no Tabuleiro 1D ou na Fita
+int K;
 
-void backtrack( int c, int t ) {
-    // fprintf(stderr, "  backtrack( %d,%d )\n", c, t );
-    if ( c ) {
-        for ( int f = 0; f < M.size(); ++f ) {
-            peca_t it = M[ f ];
-            int i = it.a;
-            int j = it.b;
-            int p = it.id;
-            for ( int vira = 0; vira < 2; ++vira ) {
-                if (( i == t || c == K ) && p ) {
-                    S[ ( c - 1 ) ] = { i, j, -1 };
-                    M[ f ].id = 0;
-                    backtrack( c - 1, j );
-                    M[ f ].id = 1;
-                }
-                std::swap( i, j );
-            }
-        }
-    } else {
-        std::vector<peca_t> s;
+// Todas as Pecas de Domino
+peca_t *PECAS;
+
+// Tabuleiro 1D ou Fita para encaixar os dominos
+peca_t *FITA;
+
+// Armazenara os resultados obtidos
+std::vector<std::vector<peca_t> > RESULTADOS;
+
+/**
+ * Funcao que faz backtrack para tentar todas as possibilidades de combinacao.
+ *
+ * @param posicao       Posicao que a peca será colocada
+ * @param last_peca     Valor da ultima peca para combinar
+ */
+void DOMINO( const int posicao, const int last_peca ) {
+    // Se completou a FITA, entao é uma possibilidade!
+    if ( posicao <= 0 ) {
+        std::vector<peca_t> resp;
         for ( int i = 0; i < K; i++ )
-            s.push_back(( peca_t ) { S[ i ].b, S[ i ].a, 0 } );
-        R.push_back( s );
+            resp.push_back(( peca_t ) { FITA[ i ].a, FITA[ i ].b, PECA_USADA } );
+        RESULTADOS.push_back( resp );
+
+        if ( V ) {
+            fprintf(stderr, "      " );
+            for ( int k = 0; k < K; ++k )
+                fprintf(stderr, "%d.%d  ", resp[ k ].a, resp[ k ].b );
+            fprintf(stderr, "\n" );
+        }
+        return;
+    }
+
+    for ( int vez = 0; vez < N; ++vez ) { // vez in 0..N
+
+        peca_t peca = PECAS[ vez ];
+        int peca_a = peca.a;
+        int peca_b = peca.b;
+        int peca_usada = peca.usado;
+
+        for ( int lado = 0; lado < 2; ++lado ) {
+            if ( V ) {
+                std::string espacos;
+                for ( int i = posicao; i < K; ++i )
+                    espacos.append( "  " );
+                fprintf(stderr, "%s%d: %d.%d\n", espacos.c_str(), vez, peca_a, peca_b );
+            }
+
+            if (( peca_usada == PECA_NAO_USADA ) && // se a peca nao foi usada ainda E
+                ( peca_a == last_peca ||            // o lado A bate com a ultima
+                  posicao == K )) {                 // OU se é a primeira peca e deve ser usada
+
+                // usando a peca
+                FITA[ ( K - posicao ) ] = { peca_a, peca_b, PECA_USADA };
+                PECAS[ vez ].usado = PECA_USADA;
+
+                // BACKTRACK
+                DOMINO( posicao - 1, peca_b );
+
+                // liberando a peca
+                PECAS[ vez ].usado = PECA_NAO_USADA;
+            }
+            // invertendo a peca
+            std::swap( peca_a, peca_b );
+        }
     }
 }
 
+/**
+ * Imprime os resultados
+ */
 void print_resultado() {
-    // std::cerr << "|R|: " << R.size() << std::endl;
-    for ( int it = 0; it < R.size(); ++it ) {
-        // std::cerr << "  " << it << ": ";
-        for ( int it2 = 0; it2 < R[ it ].size(); ++it2 ) {
-            // std::cerr << R[ it ][ it2 ].a << " "
-            //           << R[ it ][ it2 ].b << " ";
-            fprintf(stdout, "%d %d ", R[ it ][ it2 ].a, R[ it ][ it2 ].b );
-        }
-        // std::cerr << std::endl;
+    if ( V ) fprintf(stderr, "|RESULTADOS|: %d\n", ( int ) RESULTADOS.size());
+//    for ( int resp = 0; resp < RESULTADOS.size(); ++resp ) {
+    for ( std::vector<peca_t> resultado: RESULTADOS ) {
+        for ( int k = 0; k < K; ++k )
+            fprintf(stdout, "%d %d ", resultado[ k ].a, resultado[ k ].b );
         fprintf(stdout, "\n" );
     }
 }
 
 int main() {
-    std::cout << "eae Rafao" << std::endl;
-
+    // Ler a quantidade de Pecas e de slots da Fita
     fscanf(stdin, "%d %d", &N, &K );
-    // fprintf(stderr, "N: %d  K: %d\n", N, K );
+    if ( V ) fprintf(stderr, "N: %d\nK: %d\n", N, K );
 
-    peca_t s[K];
-    S = s;
+    //  Aloca todas as N pecas
+    peca_t pecas[N];
+    PECAS = pecas;
+
+    // Aloca a Fita/Tabuleiro1D de tamanho K
+    peca_t fita[K];
+    FITA = fita;
+
+    // Ler todas as PECAS de Domino e
     for ( int i = 0; i < N; ++i ) {
         int a, b;
         fscanf(stdin, "%d %d", &a, &b );
-        M.push_back( { a, b, i } );
+        PECAS[ i ] = { a, b, PECA_NAO_USADA };
+        if ( V ) fprintf(stderr, "  %d: %d.%d\n", i, PECAS[ i ].a, PECAS[ i ].b );
     }
 
-    // print_pecas();
-
-    backtrack( K, 0 );
-
+    DOMINO( K, 0 );
     print_resultado();
 }
-
-/*
- 4 3
- 1 4
- 2 3
- 2 1
- 5 1
-
- (a)
- 5 1 1 2 2 3
-
- (c)
- 4 1 1 2 2 3
- 3 2 2 1 1 4
- 3 2 2 1 1 5 -ok
- 5 1 1 2 2 3 -ok
-
- */
-
-
